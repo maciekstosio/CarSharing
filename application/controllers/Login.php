384087a6956458764190796cb35a049b84238e50
Maciek <maciekstosio@icloud.com>
2018-01-09 17:46:16 +0100

    add attr

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 	public function index() {
-        $this->load->view('partial/header');
-        $this->load->view('login');
-        $this->load->view('partial/footer');
-	}
+                $this->load->view('partial/header');
+                $this->load->view('login');
+                $this->load->view('partial/footer');
+        }
 }
