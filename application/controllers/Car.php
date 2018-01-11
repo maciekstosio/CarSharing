cc6fa57797a6fec6c9d57ce2e85c56c65e883f40
Piotr <p1619b@gmail.com>
2018-01-11 23:14:18 +0100

    parsing calendar data and inserting new availabilities

--- application/controllers/Car.php
+++ application/controllers/Car.php
@@ -10,8 +10,10 @@ class Car extends CI_Controller {
     
     public function add() {
         if (!logged_id()) redirect("user");
+        $this->load->model('Car_model');
+        $data['brand']=$this->Car_model->get_possible_brands();
         $this->load->view('partial/header');
-        $this->load->view('car_form');
+        $this->load->view('car_form',$data);
         $this->load->view('partial/footer');
     }
 
@@ -19,6 +21,45 @@ class Car extends CI_Controller {
         if (!logged_id()) redirect("user");
         echo '<pre>';
         var_dump($this -> input -> post());
+        $parsed_calendar=$this->parse_calendar($this -> input -> post());
+        if($parsed_calendar!=0){
+            $this->parse_from_json($parsed_calendar);
+        }else{
+                                        //uzupelnic
+        }
         echo '</pre>';
+    }
+
+    private function parse_calendar($input){
+        $input=$input['calendar'];
+        $len=strlen($input);
+
+        if($len==0){
+            return 0;
+        }else{
+            $input=substr($input,11,$len-13);
+            $parsed_array=explode("},",$input);
+
+            foreach($parsed_array as $key => $string){
+                if(strcmp(substr($string,-1),"}")!=0){
+                    $parsed_array[$key]= $string . "}";
+                }
+            }
+        }
+        return($parsed_array);
+    }
+
+    private function parse_from_json($parsed_calendar){
+        $parsed = array();
+        $this->load->model('Car_model');
+        foreach($parsed_calendar as $a){
+            $parsed[] = (json_decode($a,1));
+        }
+        if($this->Car_model->insert_new_availabilities($parsed)){
+            echo "ok";
+        }else{
+            echo "not ok";
+        }
+    
     }
 }
---
+++ application/models/Car_model.php
@@ -0,0 +1,43 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Car_model extends CI_Model {
+    public function get_possible_brands() {
+        if ($query = $this->db->query("SELECT DISTINCT name FROM brands")) {
+            if ($query->num_rows() >0) {
+                return $query->result();
+            }else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    public function insert_new_availabilities($parsed){
+
+        $this->db->query("START TRANSACTION");
+
+         foreach($parsed as $key => $big){
+             
+            if(strcmp($big['title'],"Available")==0){
+                 $active = "1";
+            }else{
+                 $active = "0";
+            }
+            $date = substr($big['end'],0,10);
+            $start = substr($big['start'],11,5);
+            $end = substr($big['end'],11,5);
+               
+            if(!$query = $this->db->query("INSERT INTO availability VALUES(NULL, ?, ?, ?, ?, ?)", array("DW 5555",$date, $start, $end, $active))){
+                $error = $this->db->error();
+                $this->db->query("ROLLBACK");
+                return 0;
+            }
+        }
+
+        $this->db->query("COMMIT");
+        return 1;
+
+    }
+}
+
+?>
