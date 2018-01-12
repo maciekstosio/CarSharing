581f1290d33bddd4f037b86d0481fc2fa5b8a9bd
Maciek <maciekstosio@icloud.com>
2018-01-12 12:58:18 +0100

    Fix user name in left corner and add min and max price

--- application/views/car_form.php
+++ application/views/car_form.php
@@ -19,7 +19,7 @@
             </div>
             <div>
                 <div class="uk-margin">
-                    <input class="uk-input" type="number" placeholder="Price">
+                    <input class="uk-input" type="number" min="0" max="999" placeholder="Price">
                 </div>
             </div>
         </div>
